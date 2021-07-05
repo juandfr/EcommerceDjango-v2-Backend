4db75f0c30cf20d0d6626957e42309d17f79fc62
Juan França <juanfrancavida@gmail.com>
2021-07-05 11:44:25 -0300

    first commit

---
+++ product/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+
+from .models import Category, Product
+
+admin.site.site_header = 'E-commerce'
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category', 'slug',
+                    'price', 'image', 'thumbnail')
+    search_fields = ('name',)
+    list_filter = (
+        'category__name',
+    )
+
